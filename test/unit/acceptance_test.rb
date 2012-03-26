d2c9fc778c8599eac5f7d6ae2db365eaffec2016
Francis Hwang <sera@fhwang.net>
2012-03-26 19:43:44 -0400

    test cleanup

--- test/unit/acceptance_test.rb
+++ test/unit/acceptance_test.rb
@@ -1,40 +1,44 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '/../test_helper'))
 
 class AcceptanceTest < Test::Unit::TestCase
+  def build_subclass(&block)
+    @klass = Class.new(::ActiveRecord::Base)
+    @klass.instance_eval &block
+  end
+
+  def assert_validations_json(expected)
+    assert_equal(expected, JSON.parse(@klass.validations_json))
+  end
+
   def test_simple
-    klass = Class.new(::ActiveRecord::Base) do
+    build_subclass do
       validates_acceptance_of :terms_of_service
     end
-    assert_equal(
-      {'terms_of_service' => {'acceptance' => true}},
-      JSON.parse(klass.validations_json)
-    )
+    assert_validations_json({'terms_of_service' => {'acceptance' => true}})
   end
 
   def test_message
-    klass = Class.new(::ActiveRecord::Base) do
+    build_subclass do
       validates_acceptance_of :terms_of_service, :message => 'must be abided'
     end
-    assert_equal(
+    assert_validations_json(
       {
         'terms_of_service' => {
           'acceptance' => {'message' => 'must be abided'}
         }
-      },
-      JSON.parse(klass.validations_json)
+      }
     )
   end
 
   def test_multiple_attributes
-    klass = Class.new(::ActiveRecord::Base) do
+    build_subclass do
       validates_acceptance_of :terms_of_service, :privacy_policy
     end
-    assert_equal(
+    assert_validations_json(
       {
         'terms_of_service' => {'acceptance' => true},
         'privacy_policy' => {'acceptance' => true}
-      },
-      JSON.parse(klass.validations_json)
+      }
     )
   end
 end
