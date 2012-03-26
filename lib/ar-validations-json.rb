6a1203a470b9189703a71c15d1a1aebc73955221
Francis Hwang <sera@fhwang.net>
2012-03-26 08:17:14 -0400

    more acceptance validation stuff

--- lib/ar-validations-json.rb
+++ lib/ar-validations-json.rb
@@ -11,8 +11,14 @@ module ActiveRecord
         validators.each do |validator|
           case validator
           when ActiveModel::Validations::AcceptanceValidator
-            json_hash[validator.attributes.first.to_s] =
-              {'acceptance' => true}
+            validator.attributes.each do |attr|
+              validator_hash = {}
+              if msg = validator.options[:message]
+                validator_hash['message'] = msg
+              end
+              validator_hash = true if validator_hash.empty?
+              json_hash[attr.to_s]['acceptance'] = validator_hash
+            end
           end
         end
         json_hash.to_json
--- test/unit/acceptance_test.rb
+++ test/unit/acceptance_test.rb
@@ -1,16 +1,40 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '/../test_helper'))
 
 class AcceptanceTest < Test::Unit::TestCase
-  class User1 < ::ActiveRecord::Base
-    self.table_name = 'user'
-
-    validates_acceptance_of :terms_of_service
-  end
-
   def test_simple
+    klass = Class.new(::ActiveRecord::Base) do
+      validates_acceptance_of :terms_of_service
+    end
     assert_equal(
       {'terms_of_service' => {'acceptance' => true}},
-      JSON.parse(User1.validations_json)
+      JSON.parse(klass.validations_json)
+    )
+  end
+
+  def test_message
+    klass = Class.new(::ActiveRecord::Base) do
+      validates_acceptance_of :terms_of_service, :message => 'must be abided'
+    end
+    assert_equal(
+      {
+        'terms_of_service' => {
+          'acceptance' => {'message' => 'must be abided'}
+        }
+      },
+      JSON.parse(klass.validations_json)
+    )
+  end
+
+  def test_multiple_attributes
+    klass = Class.new(::ActiveRecord::Base) do
+      validates_acceptance_of :terms_of_service, :privacy_policy
+    end
+    assert_equal(
+      {
+        'terms_of_service' => {'acceptance' => true},
+        'privacy_policy' => {'acceptance' => true}
+      },
+      JSON.parse(klass.validations_json)
     )
   end
 end
