51fba1ac6bce01b0f9eabef04afd24ff15f9b3fb
Francis Hwang <sera@fhwang.net>
2012-03-26 20:04:57 -0400

    last of acceptance validation functionality

--- lib/ar-validations-json.rb
+++ lib/ar-validations-json.rb
@@ -7,21 +7,38 @@ module ActiveRecord
 
     module ClassMethods
       def validations_json
+        Serializer.new(self).to_json
+      end
+    end
+
+    class Serializer
+      def initialize(ar_class)
+        @ar_class = ar_class
+      end
+
+      def to_json
         json_hash = Hash.new { |h,k| h[k] = {} }
-        validators.each do |validator|
+        @ar_class.validators.each do |validator|
           case validator
           when ActiveModel::Validations::AcceptanceValidator
             validator.attributes.each do |attr|
-              validator_hash = {}
-              if msg = validator.options[:message]
-                validator_hash['message'] = msg
-              end
-              validator_hash = true if validator_hash.empty?
-              json_hash[attr.to_s]['acceptance'] = validator_hash
+              json_hash[attr.to_s]['acceptance'] = validator_hash(validator)
             end
           end
         end
         json_hash.to_json
+      end
+
+      def validator_hash(validator)
+        validator_hash = {}
+        options = %w(accept message on allow_nil)
+        options.each do |option|
+          if validator.options.has_key?(option.to_sym)
+            validator_hash[option] = validator.options[option.to_sym]
+          end
+        end
+        validator_hash = true if validator_hash.empty?
+        validator_hash
       end
     end
   end
--- test/unit/acceptance_test.rb
+++ test/unit/acceptance_test.rb
@@ -14,7 +14,11 @@ class AcceptanceTest < Test::Unit::TestCase
     build_subclass do
       validates_acceptance_of :terms_of_service
     end
-    assert_validations_json({'terms_of_service' => {'acceptance' => true}})
+    assert_validations_json(
+      'terms_of_service' => {
+        'acceptance' => {'allow_nil' => true, 'accept' => '1'}
+      }
+    )
   end
 
   def test_message
@@ -22,9 +26,9 @@ class AcceptanceTest < Test::Unit::TestCase
       validates_acceptance_of :terms_of_service, :message => 'must be abided'
     end
     assert_validations_json(
-      {
-        'terms_of_service' => {
-          'acceptance' => {'message' => 'must be abided'}
+      'terms_of_service' => {
+        'acceptance' => {
+          'message' => 'must be abided', 'allow_nil' => true, 'accept' => '1'
         }
       }
     )
@@ -35,9 +39,59 @@ class AcceptanceTest < Test::Unit::TestCase
       validates_acceptance_of :terms_of_service, :privacy_policy
     end
     assert_validations_json(
-      {
-        'terms_of_service' => {'acceptance' => true},
-        'privacy_policy' => {'acceptance' => true}
+      'terms_of_service' => {
+        'acceptance' => {'allow_nil' => true, 'accept' => '1'}
+      },
+      'privacy_policy' => {
+        'acceptance' => {'allow_nil' => true, 'accept' => '1'}
+      }
+    )
+  end
+
+  def test_on_create
+    build_subclass do
+      validates_acceptance_of :terms_of_service, :on => :create
+    end
+    assert_validations_json(
+      'terms_of_service' => {
+        'acceptance' => {
+          'on' => 'create', 'allow_nil' => true, 'accept' => '1'
+        }
+      }
+    )
+  end
+
+  def test_on_update
+    build_subclass do
+      validates_acceptance_of :terms_of_service, :on => :update
+    end
+    assert_validations_json(
+      'terms_of_service' => {
+        'acceptance' => {
+          'on' => 'update', 'allow_nil' => true, 'accept' => '1'
+        }
+      }
+    )
+  end
+
+  def test_allow_nil
+    build_subclass do
+      validates_acceptance_of :terms_of_service, :allow_nil => false
+    end
+    assert_validations_json(
+      'terms_of_service' => {
+        'acceptance' => {'allow_nil' => false, 'accept' => '1'}
+      }
+    )
+  end
+
+  def test_accept
+    build_subclass do
+      validates_acceptance_of :terms_of_service, :accept => true
+    end
+    assert_validations_json(
+      'terms_of_service' => {
+        'acceptance' => {'allow_nil' => true, 'accept' => true}
       }
     )
   end
