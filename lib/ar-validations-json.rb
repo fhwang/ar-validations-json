cf32480e7e85509b1074cc7aa4445c9b38e1ec92
Francis Hwang <sera@fhwang.net>
2012-03-26 20:44:23 -0400

    validates_exclusion_of

--- lib/ar-validations-json.rb
+++ lib/ar-validations-json.rb
@@ -19,18 +19,24 @@ module ActiveRecord
       def to_json
         json_hash = Hash.new { |h,k| h[k] = {} }
         @ar_class.validators.each do |validator|
-          label =
-            validator.class.name.split(/::/).last.underscore.split(/_/).first
-          validator.attributes.each do |attr|
-            json_hash[attr.to_s][label] = validator_hash(validator)
+          if serializable?(validator)
+            label = validator.class.name.split(/::/).last.
+                    underscore.split(/_/).first
+            validator.attributes.each do |attr|
+              json_hash[attr.to_s][label] = validator_hash(validator)
+            end
           end
         end
         json_hash.to_json
       end
 
+      def serializable?(validator)
+        !(validator.options[:in] && validator.options[:in].is_a?(Proc))
+      end
+
       def validator_hash(validator)
         validator_hash = {}
-        options = %w(accept message on allow_nil)
+        options = %w(accept allow_blank allow_nil in on message)
         options.each do |option|
           if validator.options.has_key?(option.to_sym)
             validator_hash[option] = validator.options[option.to_sym]
---
+++ test/unit/exclusion_test.rb
@@ -0,0 +1,50 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '/../test_helper'))
+
+class ExclusionTest < Test::Unit::TestCase
+  def test_in_array
+    build_subclass do
+      validates_exclusion_of :username, :in => %w(admin superuser)
+    end
+    assert_validations_json(
+      'username' => {'exclusion' => {'in' => ['admin', 'superuser']}}
+    )
+  end
+
+  def test_in_range
+    build_subclass do
+      validates_exclusion_of :age, :in => (1..17)
+    end
+    assert_validations_json(
+      'age' => {
+        'exclusion' => {'in' => [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]}
+      }
+    )
+  end
+
+  def test_in_lambda
+    # Using a lambda only became an option as of ActiveRecord 3.1
+    unless ActiveRecord::VERSION::MINOR == 0
+      build_subclass do
+        validates_exclusion_of(
+          :password, :in => lambda { |p| [p.username, p.first_name] }
+        )
+      end
+      assert_validations_json({})
+    end
+  end
+
+  def test_allow_blank
+    build_subclass do
+      validates_exclusion_of :age, :allow_blank => true, :in => (1..17)
+    end
+    assert_validations_json(
+      'age' => {
+        'exclusion' => {
+          'allow_blank' => true,
+          'in' => [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+        }
+      }
+    )
+  end
+end
+
